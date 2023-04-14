494ab0143c0a008b286bf439ddb43034ce7fc67c
Peter Yockey <peterjyockey@gmail.com>
2023-04-14 18:02:58 -0500

    added local storage and box for 2nd API

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,62 @@
-var searchBtnEl = document.querySelector('#search-btn');
-var userInputEl = document.querySelector('#input-val');
+var searchBtnEl = document.querySelector("#search-btn");
+var userInputEl = document.querySelector("#input-val");
+var historyTab = document.querySelector('.historyTab')
+
+// getHistory()
+
+var cityArr = JSON.parse(localStorage.getItem("city")) || []
 
 // event listener for search button
-searchBtnEl.addEventListener('click', function() {
-  var cityName = userInputEl.value
-  getApi(cityName)
+searchBtnEl.addEventListener("click", function () {
+  var cityName = userInputEl.value;
+  cityArr.push(cityName)
+  localStorage.setItem("city", JSON.stringify(cityArr))
+  getApi(cityName);
+  addCityBtn();
 });
+
+function addCityBtn() {
+  var history = JSON.parse(localStorage.getItem("city"))
+  for(let i = 0; i < history.length; i++) {
+    var cityBtn = document.createElement("button")
+    cityBtn.setAttribute("class", "btn btn-primary")
+    cityBtn.innerHTML = history[history.length - 1]
+    var cityBtnValue = cityBtn.innerHTML
+    cityBtn.addEventListener("click", function () {
+      getApi(cityBtnValue)
+    })
+    historyTab.append(cityBtn)
+    break;
+  }
+}
+
+function getHistory() {
+  var history = JSON.parse(localStorage.getItem("city"))
+  for(let i = 0; i < history.length; i++) {
+    var cityBtn = document.createElement("button")
+    cityBtn.setAttribute("class", "btn btn-primary")
+    cityBtn.innerHTML = history[i]
+    var cityBtnValue = cityBtn.innerHTML
+    cityBtn.addEventListener("click", function () {
+      getApi(cityBtnValue)
+    })
+    historyTab.append(cityBtn)
+  }
+}
 
 // function to retrieve and use weather API
 function getApi(cityName) {
-    
-    var geocodeUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=d11a010b66ec76b6b42b5dfdd94abada&units=imperial`;
-
-    fetch(geocodeUrl)
-    .then (function(response) {
-        return response.json();
+  var geocodeUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=d11a010b66ec76b6b42b5dfdd94abada&units=imperial`;
+  fetch(geocodeUrl)
+    .then(function (response) {
+      return response.json();
     })
-    .then(apiResults => {
-        console.log(apiResults)
-        var htmlCards =""
-        for (var i = 0; i < apiResults.list.length; i = i + 8) {
-            htmlCards += `
+    .then((apiResults) => {
+      console.log(apiResults);
+      var htmlCards = "";
+
+      for (var i = 0; i < apiResults.list.length; i = i + 8) {
+        htmlCards += `
 <div class="card-fiveday border border-secondary-subtle">
   <div class="card-body">
     <h5 class="card-title">${apiResults.list[i].dt_txt}</h5>
@@ -30,9 +66,18 @@ function getApi(cityName) {
     <p class="card-text">Wind Speed: ${apiResults.list[i].wind.speed} MPH</p>
     <p class="card-link">Humidity: ${apiResults.list[i].main.humidity} %</p>
     </div>
-</div>`
-        }
-        document.querySelector('.future-forecast').innerHTML = htmlCards;
+</div>`;
+      }
+      document.querySelector(".future-forecast").innerHTML = htmlCards;
     })
-
+    .then(function() {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d11a010b66ec76b6b42b5dfdd94abada&units=imperial`)
+      .then((response) => {
+        return response.json()
+      })
+      .then((apiResults) => {
+        console.log(apiResults)
+        
+      })
+    }) 
 }
